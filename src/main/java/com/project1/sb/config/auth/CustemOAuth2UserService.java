287373e197620a924d47af8a58f258f0aa1803ec
sbpark <been778@gmail.com>
2020-08-05 11:19:17 +0900

    실서버 반영연습?

--- src/main/java/com/project1/sb/config/auth/CustemOAuth2UserService.java
+++ src/main/java/com/project1/sb/config/auth/CustemOAuth2UserService.java
@@ -35,10 +35,7 @@ public class CustemOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
         User user = saveOrUpdate(attributes);
-
-        Optional<User> findUser = userRepository.findByEmail(user.getEmail());
-
-        httpSession.setAttribute("user", new SessionUser(findUser.get()));
+        httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),attributes.getAttributes(),attributes.getNameAttributeKey());
     }
